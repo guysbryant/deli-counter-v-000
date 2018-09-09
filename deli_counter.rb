fef647cb37dc2a66ac9f265b7178394b78e19eb6
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 20:03:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty." 
   else
-    current_line = katz_deli.collect{|guest, i| "#{i}. #{guest}"}
+    current_line = katz_deli.collect{|guest, i| "#{katz_deli.index(guest)+1}. #{guest}"}
     puts "The line is currently: #{current_line.join(" ")}"
   end
 end
